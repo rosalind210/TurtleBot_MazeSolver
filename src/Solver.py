3ed1dfcbb6cfc69f33c3b1350536669dd2371f68
Jonas Tjahjadi <saintmarching@aim.com>
2017-09-29 17:48:39 -0400

    Update Solver.py

--- src/Solver.py
+++ src/Solver.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 
 import rospy
-from stdr_msgs.msg import LaserScannerMsg
+from stdr_msgs.msg import LaserSensorMsg
+#from sensor_msgs.msg import LaserScan #for real turtlebot
 from Tkinter import *
 
 class Solver():
-	def __init__(self, scan_topic):
-		self.scan_topic_name = scan_topic
+	def __init__(self, solver):
+		self.sensor_topic_name = solver
 	
 	def __move__():
 		# put move info here
-
-	def read_scanners():
-		# put reading scanners here?
-
+		
+	def read_sensors_callback(self, msgs):
+		#read / return information
+		
+		
 	def start():
 		root = Tk()
-		rospy.Subscriber(self.scan_topic_name, LaserScan, self.scanCallback)
+		rospy.Subscriber(self.sensor_topic_name, LaserScannerMsg, self.read_sensors_callback)
 		root.mainloop()
 
 def main():
