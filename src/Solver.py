77e0c72a0b6dcee878c70446fdfe16e768a8eff4
Jonas Tjahjadi <saintmarching@aim.com>
2017-09-29 16:34:25 -0400

    Update Solver.py

--- src/Solver.py
+++ src/Solver.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import LaserScan
-from Tinter import *
+from stdr_msgs.msg import LaserScannerMsg
+from Tkinter import *
 
 class Solver():
 	def __init__(self, scan_topic):
