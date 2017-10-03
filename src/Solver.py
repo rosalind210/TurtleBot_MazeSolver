27ab2e12000eb8a38f030a1029e34fdb8b534544
Jonas Tjahjadi <saintmarching@aim.com>
2017-10-03 14:13:10 -0400

    fixed runtime issues (SEE COMMENTS)
    apparently any field or method that is invoked in Solver must be called using self.
    -method signatures need to have self as a param
    -fields have to have self before it unless used temporarily.
    -publisher also needs to have self to use in other parts of code.
    Can't do cmd_vel_pub(turn_twist), ros syntax is self.cmd_vel_pub.publish(turn_twist)
    -put your global variables as their own "self" field. That's the only way it could work.
    
    It works with stdr_simulator now but we may have gotten orientation wrong, the left, right, and front are showing flipped results.

--- src/Solver.py
+++ src/Solver.py
@@ -23,12 +23,6 @@ else:
 	print("Simulation Robot")
 	
 class Solver():
-
-	# variables to follow wall
-	min_left = .05 # in meters
-	max_left = .1
-	# variables for checking front
-	min_front = .07
 	
 	def __init__(self, sensor_topic):
 		rospy.init_node('solver')
@@ -38,44 +32,50 @@ class Solver():
 		move_forward_twist.linear.x = 0.5
 		turn_twist.angular.z = 0.5
 		## init publisher
-		cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+		self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 		# init subscriber and left forward right lists
 		self.left = []
 		self.front = []
 		self.right = []
+		
+		# variables to follow wall
+		self.min_left = .05 # in meters
+		self.max_left = .1
+		# variables for checking front
+		self.min_front = .07
 
 	# Publishes move forward or turn
-	def move(direction):
+	def move(self, direction):
 		# set up distances
 		left = direction[0]
 		right = direction[1]
 		in_front = direction[2]
 		# check if we should move forward
-		forward = __check_move__(in_front)
+		forward = self.__check_move__(in_front)
 		# check if robot needs to adjust 
-		if (forward and (left < min_left or left > max_left)):
-			__adjust__(left)
+		if (forward and (left < self.min_left or left > self.max_left)):
+			self.__adjust__(left)
 		elif (forward): # move forward 
-			cmd_vel_pub(self.move_forward)
+			self.cmd_vel_pub.publish(self.move_forward)
 		elif (not forward): # have to turn
-			cmd_vel_pub(self.turn_twist)
+			self.cmd_vel_pub.publish(self.turn_twist)
 
 	# adjust to follow left wall
-	def __adjust__(distance):
+	def __adjust__(self, distance):
 		# establish diagonal movement (only necessary here -- why its not global or init)
 		go_left = Twist()
 		go_left.angular.z = -0.2
 		go_left.linear.x = 0.2
 		# actually move
-		cmd_vel_pub(go_left)
+		self.cmd_vel_pub.publish(go_left)
 
 	# check if there is a wall in front
-	def __check_move__(front):
+	def __check_move__(self, front):
 		# check front
-		if (front < min_front):
-			return false
+		if (front < self.min_front):
+			return False
 		else:
-			return true
+			return True
 
 	def read_sensors_callback(self, msg):
 		#read / return information
@@ -83,16 +83,19 @@ class Solver():
 		print("Angle Increment is " + str(msg.angle_increment))
 		print(str(len(msg.ranges) * msg.angle_increment))
 		# start movement by calling scanners to interpret information
-		#read_scanners()
+		self.read_scanners()
 		
 
 	# should take in callback info and interpret it into forward, turn, out
 	def read_scanners():
 		self.left = min(msg.ranges[255:285])
+		print(self.left)
 		self.right = min(msg.ranges[75:105])
+		print(self.right)
 		self.front = min(zip(msg.ranges[345:360], msg.ranges[0:15]))
+		print(self.front)
 		# move given info
-		move([left, right, front])
+		self.move([self.left, self.right, self.front])
 
 	def start(self):
 		root = Tk()
@@ -105,7 +108,7 @@ def main():
 	if(robotid == "tb"):
 		maze_solver = Solver("/scan")
 	else:
-		maze_solver = Solver("/robot0/laser_0")
+		maze_solver = Solver("/robot0/laser_1")
 	maze_solver.start()
 	#TODO - create shutdown method for when maze finishes
 
